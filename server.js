f54db172e1dcf1a53711c656c5f450675356ebc5
Katie Yang <13hy22@queensu.ca>
2020-12-19 08:37:21 -0500

    fixing port

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 //importing the express module
 var express = require('express');
 var app = express();
-var server = app.listen(3000);
+var server = app.listen(process.env.PORT || 5000);
 app.use(express.static('public'));
 
 console.log("server is running");
